5883696af500d6613e6ae83a08d627005901e681
Alessandro Aria Wibowo <assandroa95@gmail.com>
2019-12-11 20:47:23 +0700

    fix predict.js

--- public/js/predict.js
+++ public/js/predict.js
@@ -19,7 +19,21 @@ $(document).ready(() => {
     $('#input-date').val(new Date().toDateInputValue());
 });
 
-function getItems() {
+async function getItems() {
+    const url = "https://dss-model-based-management.herokuapp.com/predict";
+    const payload = {date:$dateValue.value};
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+
+    json = await response.json();
+
     return [
         {itemName: "tes", quantity:"120"},
         {itemName: "tes", quantity:"120"},
