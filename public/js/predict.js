edd0fff9c995ca34176743611078bcd1e7bf48c1
Alessandro Aria Wibowo <assandroa95@gmail.com>
2019-12-11 19:14:35 +0700

    fix predict.js

--- public/js/predict.js
+++ public/js/predict.js
@@ -3,17 +3,15 @@ const $predictButton = document.querySelector("#predict-button");
 const $forecastResult = document.querySelector("#forecast-result");
 const resultTemplate = document.querySelector("#result-template").innerHTML;
 
-let items;
-
 console.log("loaded");
 $predictButton.addEventListener("click", (event) => {
     event.preventDefault();
     while ($forecastResult.firstChild) $forecastResult.removeChild($forecastResult.firstChild);
-
     const value = $dateValue.value;
     console.log(`date value: ${value}`);
-    items = getItems();
-    const html = Mustache.render(resultTemplate,{});
+
+    const items = getItems();
+    const html = Mustache.render(resultTemplate,{items});
     $forecastResult.insertAdjacentHTML("beforeend", html);
 });
 
@@ -23,19 +21,9 @@ $(document).ready(() => {
 
 function getItems() {
     return [
-        {'foodName': 'Bakso Goreng',
-            'ingredients': [
-                {'ingredientName': 'Daging', 'quantity': 5830, 'metric': 'gram'},
-                {'ingredientName': 'Bawang', 'quantity': 5830, 'metric': 'gram'}]},
-        {'foodName': 'Mie Ayam',
-            'ingredients': [
-                {'ingredientName': 'Mie', 'quantity': 34200, 'metric': 'gram'},
-                {'ingredientName': 'Kuah', 'quantity': 3420, 'metric': 'liter'}]},
-        {'foodName': 'Bakso Keju',
-            'ingredients': [
-                {'ingredientName': 'Daging', 'quantity': 3290, 'metric': 'gram'},
-                {'ingredientName': 'Keju', 'quantity': 3290, 'metric': 'gram'}]
-        }];
+        {itemName: "tes", quantity:"120"},
+        {itemName: "tes", quantity:"120"},
+        {itemName: "tes", quantity:"120"}];
 }
 
 Date.prototype.toDateInputValue = (function() {
